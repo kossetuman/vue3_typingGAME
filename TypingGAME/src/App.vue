4c274260b2011f9f5f523e6368ec04e85f59cd24
kossetuman <ooutim2020@gmail.com>
2024-11-09 03:44:02 +0900

    ref()

---
+++ TypingGAME/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+const title = 'vue.js!'
+let price = ref(9.9)
+function increment() {
+  price.value = price.value + 1
+}
+</script>
+<template>
+  <h1>{{ title }}</h1>
+  <h2>${{ price - 1 }}</h2>
+  <button @click="increment">button</button>
+</template>
